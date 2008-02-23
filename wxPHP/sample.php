2028806b7403a730417a4f21f5a0cad5ef1db8d8
seariver <seariver@5401d555-a92c-0410-8172-d1cd23e46aa8>
2008-02-23 21:19:50 +0000

    Added a script sample
    git-svn-id: http://wxphp.svn.sourceforge.net/svnroot/wxphp/trunk@4 5401d555-a92c-0410-8172-d1cd23e46aa8

---
+++ wxPHP/sample.php
@@ -0,0 +1,73 @@
+<?php
+	dl('php_wxWidgets.dll');
+	
+	class mainFrame extends wxFrame
+	{
+		function onQuit()
+		{
+			$this->Destroy();
+		}
+		
+		function onAbout()
+		{
+			$dlg = new wxMessageDialog($this,"Welcome to wxPHP!!\nBased on wxWidgets 2.8.4\n\nThis is a minimal wxPHP sample!","About box...",wxICON_INFORMATION);
+			$dlg->ShowModal();
+		}
+	
+		function mainFrame()
+		{
+			parent::__construct(null ,null,"Minimal wxPHP App",wxDefaultPosition, new wxSize(350,260));
+			$ico = new wxIcon();
+			$ico->CopyFromBitmap(new wxBitmap("sample.xpm",wxBITMAP_TYPE_XPM));
+			$this->SetIcon($ico);
+			
+			$mb = new wxMenuBar();
+			
+			$mn = new wxMenu();
+			$mn->Append(2,"E&xit","Quit this program");
+			$mb->Append($mn,"&File");
+			
+			$mn = new wxMenu();
+			$mn->AppendCheckItem(4,"&About...","Show about dialog");
+			$mb->Append($mn,"&Help");
+			
+			$this->SetMenuBar($mb);
+			
+			$sbar = $this->CreateStatusBar(2);
+			$sbar->SetStatusText("Welcome to wxPHP...");
+			
+			$this->Connect(2,wxEVT_COMMAND_MENU_SELECTED,array($this,"onQuit"));
+			$this->Connect(4,wxEVT_COMMAND_MENU_SELECTED,array($this,"onAbout"));
+		}
+	}
+	
+	
+	class myApp extends wxApp 
+	{
+		function OnInit()
+		{
+			global $mf;
+			
+			$mf = new mainFrame();
+			$mf->Show();
+			
+			return true;
+		}
+		
+		function OnExit()
+		{
+			
+			echo "Application terminated";
+			return 0;
+		}
+	}
+
+	$xt = new myApp();
+
+	if(wxInitialize())
+	{
+		wxApp::SetInstance($xt);
+		wxEntry();
+	}	
+
+?>
