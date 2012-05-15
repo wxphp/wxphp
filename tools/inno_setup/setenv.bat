0b438cd44a6e3f5e6cf42d58d17e3a1873a513b5
jgmdev <jgmdev@gmail.com>
2012-05-15 18:55:54 -0400

    Added Inno Setup script to generate windows installer

---
+++ tools/inno_setup/setenv.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+set Path=%Path%;wxPHP\php
+
+echo wxPHP Command Prompt
+echo ===========================================================
+echo You can execute php scripts with wxWidgets code like this:
+echo.
+echo     wxphp script.php
+echo.
+echo ===========================================================
---
+++ tools/inno_setup/shell.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+cmd /K setenv.bat
