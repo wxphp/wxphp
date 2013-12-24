f81044cc86f05e1b4d8f905f543973a3482a557f
jgmdev <jgmdev@gmail.com>
2013-12-23 23:07:34 -0400

    Fixes to debian packaging script and control file.

--- debian/build-script.sh
+++ debian/build-script.sh
@@ -6,20 +6,19 @@ CPU_CORES=`grep processor /proc/cpuinfo | wc -l`
 CORES_COUNT=$[(($CPU_CORES / 2) + 1)]
 
 # Clone wxWidgets from github if not available
-if [ ! -e "wxWidgets.git" ]; then
-    git clone https://github.com/wxWidgets/wxWidgets.git wxWidgets.git
+if [ ! -e "wxWidgets-3.0.0" ]; then
+    wget http://downloads.sourceforge.net/wxwindows/wxWidgets-3.0.0.tar.bz2
+    tar -xjf wxWidgets-3.0.0.tar.bz2
+    rm wxWidgets-3.0.0.tar.bz2
 fi
 
 # Build wxWidgets if not already build
-cd wxWidgets.git
-
-# Build wxWidgets and install it locally
-git checkout tags/WX_3_0_0
+cd wxWidgets-3.0.0
 
 mkdir mybuild
 cd mybuild
 
-WX_BUILD_DIR=`pwd | sed "s/wxWidgets.git\/mybuild//"`wxWidgets-build
+WX_BUILD_DIR=`pwd | sed "s/wxWidgets-3.0.0\/mybuild//"`wxWidgets-build
 
 CFLAGS="-fPIC -O2 -Wall -W" CXXFLAGS="-fPIC -O2"  \
 ../configure --prefix=$WX_BUILD_DIR --disable-shared --enable-monolithic
@@ -35,7 +34,7 @@ php tools/reference_generator.php
 # Generate configure script
 phpize5
 
-WX_LDFLAGS="-LwxWidgets-build/lib -pthread -lwx_gtk2u-3.0 -lwx_gtk2u_gl-3.0 -lwxregexu-3.0 -lwxscintilla-3.0"
+WX_LDFLAGS="-LwxWidgets-build/lib -pthread -lwx_gtk2u-3.0 -lwxregexu-3.0 -lwxscintilla-3.0"
 OTHER_LDFLAGS=`pkg-config --libs gstreamer-0.10 gstreamer-interfaces-0.10 gtk+-2.0  libpng zlib`
 
 dh_auto_configure -- \
