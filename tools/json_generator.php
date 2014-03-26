2acc08a33e8363631e33bb4c55b8771adf419de2
jgmdev <jgmdev@gmail.com>
2014-03-26 16:24:50 -0400

    Manual merge to remove unnecessary json_indent from json_generator.php since the functionality it provided is already part of PHP which closes #55.

--- tools/json_generator.php
+++ tools/json_generator.php
@@ -50,70 +50,9 @@ $file_not_handle = array();
 //Store base classes to check which ones aren't documented
 $base_classes = array();
 
-/**
- * Indents a flat JSON string to make it more human-readable.
- * URL: http://recursive-design.com/blog/2008/03/11/format-json-with-php/
- *
- * @param string $json The original JSON string to process.
- *
- * @return string Indented version of the original JSON string.
- */
-function json_indent($json) {
-
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = "\t";
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
-
-    for ($i=0; $i<=$strLen; $i++) {
-
-        // Grab the next character in the string.
-        $char = substr($json, $i, 1);
-
-        // Are we inside a quoted string?
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
-        
-        // If this character is the end of an element, 
-        // output a new line and indent the next line.
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        // Add the character to the result string.
-        $result .= $char;
-
-        // If the last character was the beginning of an element, 
-        // output a new line and indent the next line.
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-            
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        $prevChar = $char;
-    }
-
-    return $result;
-}
-
 function serialize_json($data)
-{
-	$data = json_encode($data);
-	
-	return json_indent($data);
+{	
+	return json_encode($data, JSON_PRETTY_PRINT);
 }
 
 $doc = new DOMDocument();
