76ba31ac7dd911af0a80e736c2bd81b0d1ce3984
Jon Hinks <projects.ws@jondh.me.uk>
2016-01-31 12:10:20 +0000

    Log another minor bug

--- examples/AUI/main.php
+++ examples/AUI/main.php
@@ -17,6 +17,9 @@
  *       wxPHP segmentation fault - I expect this would be less of a problem if pane updates
  *       were done in a better fashion (see pane.php:onPaneTickBoxChange) but this issue
  *       would be good to fix C-side anyway
+ * @todo An existing bug: for a pane that has a top gripper at start up, if the gripper flag
+ * is turned off and then on again, the gripper moves to the side even though the top flag
+ * is on. However toggling the top flag stops this behaviour. Needs some looking into.
  */
 
 namespace WxPhpExamples\AUI;
